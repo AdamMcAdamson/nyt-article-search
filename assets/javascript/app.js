7cc925a68996e43aecfc51f2c8595da8a558c57c
Kyleds63 <kstoner63@gmail.com>
2017-03-27 11:19:22 -0500

    Kyle

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
-// api key - 973e6b52b32e4212b225f59d1af1b44f
+var apiKey = "973e6b52b32e4212b225f59d1af1b44f";
 
-// TEMP VALUES
-// --------------------------
+// Required search form inputs
 var searchTerm;
-
 var numRecords;
 
+// Optional search form inputs
 var startYear;
 var endYear;
 
-var searchArticleNumber = 0;
+// Total number of articles searched
+var articleNumber = 0;
 
-// --------------------------
 
-// Article array
+// Article array from API call
 var articleArr;
 
+// Articles div element in DOM
 var articlesDiv;
 
 // Number of ajax calls (# of pages)
@@ -23,38 +23,44 @@ var numCalls;
 
 
 $( document ).ready(function(){
-
+	// Grab DOM element 
 	articlesDiv = $("#articles");
 
+	// When Search button is clicked, grab form info 
+	// and call processing function makeRequests
 	$("#search-button").on("click", function(){
 		var keywords = $("#search-terms").val().trim();
 		var numRecords = parseInt($("#num-drop").find(":selected").text());
 		var start = $("#year-start").val().trim();
 		var end = $("#year-end").val().trim();
 
+		// Search and Processing function
 		makeRequests(keywords, numRecords, start, end);
 	});
 
+	// Clear previous search results and reset counter
 	$("#clear-button").on("click", function(){
+		searchArticleNumber = 0;
 		articlesDiv.empty();
 	});
 
-
-
 });
 
+// Search query and Processing function
 function makeRequests(keywords, num, start, end){
 
+	// Required form Info
 	searchTerm = keywords;
-
 	numRecords = num;
 
+	// Optional form info
 	startYear = start || null;
-
 	endYear = end || null;
 
+	// For checking proper input
 	var startYearNum, endYearNum;
 	
+	// Check for proper startYear input
 	if(startYear !== null){
 		startYearNum = parseInt(startYear);
 		if(startYearNum.toString.length !== 4){
@@ -62,7 +68,7 @@ function makeRequests(keywords, num, start, end){
 		}
 	}
 
-
+	// Check for proper endYear input
 	if(endYear !== null){
 		endYearNum = parseInt(endYear);
 		if(endYearNum.toString.length !== 4){
@@ -70,18 +76,20 @@ function makeRequests(keywords, num, start, end){
 		}
 	}
 
-	// Parameters to search with
+	// Default Parameters to search with
 	var searchParameters = $.param({
-		'api-key': '973e6b52b32e4212b225f59d1af1b44f',
+		'api-key': apiKey,
 		'q': searchTerm,
 	});
 
+	// Check optional param and handle
 	if(startYear !== null){
 		searchParameters += $.param({
 			'begin_date': startYear + "0101"
 		});
 	}
 
+	// Check optional param and handle
 	if(endYear !== null){
 		searchParameters += $.param({
 			'end_date': endYear + "1231"
@@ -104,12 +112,8 @@ function makeRequests(keywords, num, start, end){
 		// Loop through each article
 		for(var j = 0; j < numRecords; j++){
 
-			// The array number in the DOM 
-			// Broken b/c asyncronous
-			// var articleNumber = (i * 10) + j + 1;
-			
-			// temp articleNumber
-			var articleNumber = searchArticleNumber + j+1;
+			// Exact article number, includes other searches
+			articleNumber++;
 
 
 
@@ -119,55 +123,55 @@ function makeRequests(keywords, num, start, end){
 			// Article title (String)
 			var articleTitle = artCur.headline.main;
 
-			// Author (By JOHN SMITH)
+			// Author ("By JOHN SMITH")
 			var author = artCur.byline.original;
 
-			// Article section (Real Estate)
+			// Article section ("Real Estate")
 			var section = artCur.section_name;
 
-			// Publication Date (2010-08-04T05:00:59Z)
+			// Publication Date ("2010-08-04T05:00:59Z")
 			var pubDate = artCur.pub_date;
 
-			// Link to full Article
+			// Link to full Article ("http://temp.example.com")
 			var webLink = artCur.web_url;
 
 
 
 			// Debug Output
-			console.log("__________ARTICLE_________");
-			console.log("Number: " + articleNumber);
-			console.log("Title: " + articleTitle);
-			console.log("Author: " + author);
-			console.log("Section: " + section);
-			console.log("Date: " + pubDate);
-			console.log("Link: " + webLink);
-			console.log("__________________________");
+			// console.log("\n\n_____________CLEAR______________\n\n");
+			// console.log("__________ARTICLE_________");
+			// console.log("Number: " + articleNumber);
+			// console.log("Title: " + articleTitle);
+			// console.log("Author: " + author);
+			// console.log("Section: " + section);
+			// console.log("Date: " + pubDate);
+			// console.log("Link: " + webLink);
+			// console.log("__________________________");
 
-			console.log("\n\n_____________CLEAR______________\n\n");
-
+			// Create current article div element in jQuery
 			var artCurDiv = $("<div class='article-div' id='article-div-" + articleNumber + " '>");
 
+			// Title h3 
 			var titleTag = $("<h3>");
 
+			// Article Number
 			titleTag.append("<span class='article-num' id='article-num-" + articleNumber + "'>" + articleNumber + "</span>")
-
+			// Article Title
 			titleTag.append("<b class='article-title' id='title-" + articleNumber + "'>" + articleTitle + "</b>");
-
+			
+			// Append Article Title h3
 			artCurDiv.append(titleTag);
 
-			artCurDiv.append("<p class='byline' id='byline-" + articleNumber + "''>" + author + "</p>");
-			
+			// Append Article div
+			artCurDiv.append("<p class='byline' id='byline-" + articleNumber + "''>" + author + "</p>");	
 			artCurDiv.append("<p class='section' id='section-" + articleNumber + "''>Section: " + section + "</p>");
-
 			artCurDiv.append("<p class='date' id='date-" + articleNumber + "'>" + pubDate + "</p>");
-
 			artCurDiv.append("<a class='link' id='link-" + articleNumber + "' href='" + webLink + "'>" + webLink + "</a>");
 
+			// Write Atricle div to DOM 
 			articlesDiv.append(artCurDiv);
 
 		}
-
-		searchArticleNumber += numRecords;
 
 	});
 
