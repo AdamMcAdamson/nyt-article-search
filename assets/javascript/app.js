68c3888826cee6b01bac20aefb0dcca6079657f6
Adam Brunn <adambrunn1@hotmail.com>
2017-03-27 09:43:49 -0500

    Logic progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,113 +22,143 @@ var numCalls;
 $( document ).ready(function(){
 
 
-	articlesDiv = $("#articles");
+	$("#search-button").on("click", function(){
+		var keywords = $("#search-terms").val().trim();
+		var numRecords = $("#num-drop").find(":selected").text();
+		var start = $("#year-start").val().trim();
+		var end = $("#year-end").val().trim();
 
-	numCalls = Math.floor(numRecords/10) + 1;
-	
+		makeRequests(keywords, numRecords, start, end);
+	});
+
+	articlesDiv = $("#articles");
 
 }
 
-function makeRequests(){
-	// Loop to make requests
-	for(var i = 0; i < numCalls; i++){
+function makeRequests(keywords, num, start, end){
 
-		// Number of articles to use in this request
-		var numToUse = numRecords;
-		if(numToUse > 10){
-			numToUse = 10;
-			numRecords -= 10;
+	searchTerm = keywords;
+
+	numRecords = num;
+
+	startYear = start || null;
+
+	endYear = end || null;
+
+	var startYearNum, endYearNum;
+	
+	if(startYear !== null){
+		startYearNum = parseInt(startYear);
+		if(startYearNum.toString.length !== 4){
+			startYear = null;
+		}
+	}
+
+
+	if(endYear !== null){
+		endYearNum = parseInt(endYear);
+		if(endYearNum.toString.length !== 4){
+			endYear = null;
+		}
+	}
+
+	// Parameters to search with
+	var searchParameters = $.param({
+		'api-key': '973e6b52b32e4212b225f59d1af1b44f',
+		'q': searchTerm,
+	});
+
+	if(startYear !== null){
+		searchParameters += $.param({
+			'begin_date': startYear + "0101"
+		});
+	}
+
+	if(endYear !== null){
+		searchParameters += $.param({
+			'end_date': endYear + "1231"
+		});
+	}
+
+	// URL to query with ajax
+	var nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + searchParameters;
+
+	// ajax request
+	$.ajax({
+		url: nytURL,
+		method: "GET"
+	}).done(function(nytResponse){
+
+		// Grab the array of articles
+		articleArr = nytResponse.response.docs;
+
+
+		// Loop through each article
+		for(var j = 0; j < numRecords; j++){
+
+			// The array number in the DOM 
+			// Broken b/c asyncronous
+			// var articleNumber = (i * 10) + j + 1;
+			
+			// temp articleNumber
+			var articleNumber = j+1;
+
+
+
+			// Current Article obj
+			var artCur = articleArr[j];
+
+			// Article title (String)
+			var articleTitle = artCur.headline.main;
+
+			// Author (By JOHN SMITH)
+			var author = artCur.byline.original;
+
+			// Article section (Real Estate)
+			var section = artCur.section_name;
+
+			// Publication Date (2010-08-04T05:00:59Z)
+			var pubDate = artCur.pub_date;
+
+			// Link to full Article
+			var webLink = artCur.web_url;
+
+
+
+			// Debug Output
+			console.log("__________ARTICLE_________");
+			console.log("Number: " + articleNumber);
+			console.log("Title: " + articleTitle);
+			console.log("Author: " + author);
+			console.log("Section: " + section);
+			console.log("Date: " + pubDate);
+			console.log("Link: " + webLink);
+			console.log("__________________________");
+
+			console.log("\n\n_____________CLEAR______________\n\n");
+
+			var artCurDiv = $("<div>");
+
+			var titleTag = $("<h3>");
+
+			titleTag.append("<span class='article-num' id='article-num-" + articleNumber + "'>" + articleNumber + "</span>")
+
+			titleTag.append("<b class='article-title' id='title-" + articleNumber + "'>" + articleTitle + "</b>");
+
+			artCurDiv.append(titleTag);
+
+			artCurDiv.append("<p class='byline' id='byline-" + articleNumber + "''>" + author + "</p>");
+			
+			artCurDiv.append("<p class='section' id='section-" + articleNumber + "''>Section: " + section + "</p>");
+
+			artCurDiv.append("<p class='date' id='date-" + articleNumber + "'>" + pubDate + "</p>");
+
+			artCurDiv.append("<a class='link' id='link-" + articleNumber + "' href='" + webLink + "'>" + webLink + "</a>");
+
+			articlesDiv.append(artCurDiv);
+
 		}
 
-		// Parameters to search with
-		var searchParameters = $.param({
-			'api-key': '973e6b52b32e4212b225f59d1af1b44f',
-			'q': searchTerm,
-			'begin_date': startYear + "0101",
-			'end_date': endYear +"1231",
-			'page': i
-		});
+	});
 
-		// URL to query with ajax
-		var nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + searchParameters;
-
-		// ajax request
-		$.ajax({
-			url: nytURL,
-			method: "GET"
-		}).done(function(nytResponse){
-
-			// Grab the array of articles
-			articleArr = nytResponse.response.docs;
-
-
-			// Loop through each article
-			for(var j = 0; j < numToUse; j++){
-
-				// The array number in the DOM 
-				// Broken b/c asyncronous
-				// var articleNumber = (i * 10) + j + 1;
-				
-				// temp articleNumber
-				var articleNumber = j+1;
-
-
-
-				// Current Article obj
-				var artCur = articleArr[j];
-
-				// Article title (String)
-				var articleTitle = artCur.headline.main;
-
-				// Author (By JOHN SMITH)
-				var author = artCur.byline.original;
-
-				// Article section (Real Estate)
-				var section = artCur.section_name;
-
-				// Publication Date (2010-08-04T05:00:59Z)
-				var pubDate = artCur.pub_date;
-
-				// Link to full Article
-				var webLink = artCur.web_url;
-
-
-
-				// Debug Output
-				console.log("__________ARTICLE_________");
-				console.log("Number: " + articleNumber);
-				console.log("Title: " + articleTitle);
-				console.log("Author: " + author);
-				console.log("Section: " + section);
-				console.log("Date: " + pubDate);
-				console.log("Link: " + webLink);
-				console.log("__________________________");
-
-				console.log("\n\n_____________CLEAR______________\n\n");
-
-				var artCurDiv = $("<div>");
-
-				var titleTag = $("<h3>");
-
-				titleTag.append("<span class='article-num' id='article-num-" + articleNumber + "'>" + articleNumber + "</span>")
-
-				titleTag.append("<b class='article-title' id='title-" + articleNumber + "'>" + articleTitle + "</b>");
-
-				artCurDiv.append(titleTag);
-
-				artCurDiv.append("<p class='byline' id='byline-" + articleNumber + "''>" + author + "</p>");
-				
-				artCurDiv.append("<p class='section' id='section-" + articleNumber + "''>Section: " + section + "</p>");
-
-				artCurDiv.append("<p class='date' id='date-" + articleNumber + "'>" + pubDate + "</p>");
-
-				artCurDiv.append("<a class='link' id='link-" + articleNumber + "' href='" + webLink + "'>" + webLink + "</a>");
-
-				articlesDiv.append(artCurDiv);
-
-			}
-
-		});
-
-	}
 }
